3e771e977cb5ea6e201a1bd74b59da8f4d454d58
Walker Sorensen <wsoren1@mc2school.org>
2020-06-28 14:38:07 -0400

    the great git init!

---
+++ main.py
@@ -0,0 +1,18 @@
+import datetime as dt
+from time import sleep
+import Interact
+# import CalendarSync
+import json
+
+
+with open('settings.json', 'r') as file:
+    settings = json.loads(file.read())
+    refresh_time = settings["refresh_time"]
+
+
+while True:  # loop that bot will run tasks
+    # Extract data
+    # run data processing script
+    # push any re-occuring tasks to appropriate board
+    # separate active tasks from previous tasks
+    sleep(settings["refresh_time"])
